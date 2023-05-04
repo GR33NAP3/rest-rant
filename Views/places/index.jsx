3f03da05a9bddcee84377108127e147bc8e82244
Eric Bunn <Sarutke93@gmail.com>
2023-05-04 14:25:47 -0400

    added a home button

--- Views/places/index.jsx
+++ Views/places/index.jsx
@@ -23,6 +23,7 @@ function index (data) {
                 <div className="row">
                     {placesFormatted}
                 </div>
+                <a href='/'>Home</a>
             </main>
         </Def>
     )
