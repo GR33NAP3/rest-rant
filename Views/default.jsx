bf93414e4ccd6b931985f45e922200e669eda6e0
Eric Bunn <Sarutke93@gmail.com>
2023-05-04 14:23:38 -0400

    adding bootstrap grid to indexpage and changed the images to local places

--- Views/default.jsx
+++ Views/default.jsx
@@ -4,6 +4,8 @@
     return (
         <html>
             <head>
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossOrigin="anonymous" />
+                <link rel="stylesheet" href="/style.css" />
                 <title>Title</title>
             </head>
             <body>
